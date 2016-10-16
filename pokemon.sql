694e36e76b4465fd9c48b9bae1002631c95d3d83
Brais Gabín <braisgabin@gmail.com>
2016-10-16 17:14:59 +0200

    Add names in fr and ge

--- pokemon.sql
+++ pokemon.sql
@@ -1,11 +1,16 @@
 CREATE TABLE pokemon (
   id INTEGER PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
+  name_de TEXT NOT NULL,
+  name_fr TEXT NOT NULL,
   atk INTEGER NOT NULL,
   def INTEGER NOT NULL,
   stam INTEGER NOT NULL,
-  candy TEXT NOT NULL
+  candy TEXT NOT NULL,
+  candy_de TEXT NOT NULL,
+  candy_fr TEXT NOT NULL
 );
 .mode csv pokemon
+.separator ;
 .import /data/list-pokemon.csv pokemon
 .quit
